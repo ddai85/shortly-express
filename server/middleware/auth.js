bde70ba1ffd45c3609189bda117162475043d37d
Hack Reactor Student <student@hackreactor.com>
2017-09-20 16:34:14 -0700

    Handle sessions depending on incoming cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,35 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (!Object.keys(req.cookies).length) {
+    return models.Sessions.create()
+      .then(result => {
+        return models.Sessions.get({id: result.insertId});
+      })
+      .then(session => {
+        req.session = {};
+        req.session['hash'] = session.hash;
+        res.cookie('shortlyid', session.hash);
+        next();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    return models.Sessions.get({hash: req.cookies.shortlyid})
+      .then(session => {
+        if (session) {
+          req.session = session;
+          next();
+        } else {
+          req.cookies = {};
+          module.exports.createSession(req, res, next);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
 
 /************************************************************/
